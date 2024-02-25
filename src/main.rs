04f1b5c7f8845f45a6cce593ac6a9da4c3b5f7ed
Raul Jordan <raul@prysmaticlabs.com>
2024-02-25 11:28:53 -0600

    fixes

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,38 @@ use ethers::{
 };
 use eyre::Result;
 use hex;
+use std::env;
 
 fn main() -> Result<()> {
-    // let phrase =
-    //     "setup desert impact cabbage special agree daughter lady midnight crumble saddle segment";
-    // let index = 0u32;
+    let args: Vec<String> = env::args().collect();
+    let wallet = if args.len() > 1 {
+        // Use provided mnemonic phrase
+        let phrase = &args[1];
+        let index = 0u32;
 
-    // // Access mnemonic phrase with password
-    // // Child key at derivation path: m/44'/60'/0'/0/{index}
-    // let wallet = MnemonicBuilder::<English>::default()
-    //     .phrase(phrase)
-    //     .index(index)?
-    //     .build()?;
-
-    // eprintln!("Wallet: {wallet:?}");
-
-    // Generate a random wallet (24 word phrase) at custom derivation path
-    let mut rng = rand::thread_rng();
-    let wallet = MnemonicBuilder::<English>::default()
-        .word_count(12)
-        .derivation_path("m/44'/60'/0'/0/0")?
-        // Optionally add this if you want the generated mnemonic to be written
-        // to a file
-        .write_to("./")
-        .build_random(&mut rng)?;
+        // Access mnemonic phrase with derivation path: m/44'/60'/0'/0/{index}
+        MnemonicBuilder::<English>::default()
+            .phrase(phrase)
+            .index(index)?
+            .build()?
+    } else {
+        // Generate a random wallet (12 word phrase) at custom derivation path
+        let mut rng = rand::thread_rng();
+        MnemonicBuilder::<English>::default()
+            .word_count(12)
+            .derivation_path("m/44'/60'/0'/0/0")?
+            .build_random(&mut rng)?
+    };
 
     let priv_key = hex::encode(wallet.signer().to_bytes());
     let addr = wallet.address();
 
-    eprintln!("Random wallet: {wallet:?}");
+    if args.len() > 1 {
+        eprintln!("Wallet from provided mnemonic: {wallet:?}");
+    } else {
+        eprintln!("Random wallet: {wallet:?}");
+    }
+    eprintln!("Address: {addr}");
     eprintln!("Privkey: {priv_key:?}");
 
     Ok(())
